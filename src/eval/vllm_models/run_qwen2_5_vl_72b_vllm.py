f5abbc666513d9da0b9a6e9d977bd3fe155127c6
Dennis Hug <dennis@example.com>
2025-12-06 18:37:10 +0100

    h

--- src/eval/vllm_models/run_qwen2_5_vl_72b_vllm.py
+++ src/eval/vllm_models/run_qwen2_5_vl_72b_vllm.py
@@ -237,8 +237,9 @@ class VLMEvaluator:
             trust_remote_code=True,
             max_model_len=4096,
             gpu_memory_utilization=0.9,
+            dtype="bfloat16",
             load_format="bitsandbytes",
-            quantization="bitsandbytes",
+            quantization_backend="bitsandbytes",
         )
         
         # Sampling Parameter erstellen - je nach vLLM Version
